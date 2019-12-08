5613d7e94f337cc4b17a7a211a7879a749a3a68c
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-12-08 10:29:24 +0300

    add cors and react port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // require neccessary NPM packages
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 // require route files
@@ -24,6 +25,7 @@ const app = express();
 
 // define a port for the API to run on 
 const port = process.env.PORT || 5000;
+const reactPort = 3000;
 
 
 /* middle ware */
@@ -35,6 +37,8 @@ the method '.use' sets up middleware for the express application
 */
 app.use(express.json());
 
+/* set the CORS headers on response from this API using the 'cors' NPM package */
+app.use(cors({origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}`}));
 
 /* Routes */
 // mount imported routers
